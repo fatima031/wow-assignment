e39cbddcd63d8d542ce5c5878748ee591ca0ac8c
fatima <fatima031>
2024-02-02 01:22:05 +0500

    my work

---
+++ assignment1/lib/main.dart
@@ -0,0 +1,241 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          
+          child:
+          Column(
+            children: [
+              SingleChildScrollView
+              (scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    
+                        ],
+                ),
+              ),
+
+              Column(
+                children: [
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                    cont(100, 75, Colors.black),
+                    cont(100, 75, Colors.grey),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ) ,
+        ),
+        
+        
+      ),
+    );
+  }
+}
+
+cont(height, width, Color) {
+  return Container(
+    height: height,
+    width: width,
+    color: Color,
+  );
+}
